24a00cfbc3bb635109b85b8448e57abc9a3d9bfd
Andrew Jones <ajones@digitalbazaar.com>
2020-02-27 11:14:31 -0500

    Add test for updated verifier route.

--- test/mocha/10-verify.js
+++ test/mocha/10-verify.js
@@ -11,7 +11,14 @@ const helpers = require('./helpers');
 const https = require('https');
 
 const strictSSL = false;
-const url = `${config.server.baseUri}/vc/verify`;
+
+const urls = {
+  verify: `${config.server.baseUri}/vc/verify`,
+  verification({verifierId, referenceId}) {
+    return `${config.server.baseUri}/verifiers/${verifierId}/` +
+      `verifications/${referenceId}`;
+  }
+};
 
 // FIXME: update to latest vc-js APIs
 describe('verify API using local DID document loader', () => {
@@ -35,7 +42,7 @@ describe('verify API using local DID document loader', () => {
         httpsAgent: new https.Agent({rejectUnauthorized: strictSSL}),
         data: {challenge, domain, presentation},
         method: 'POST',
-        url,
+        url: urls.verify,
       });
     } catch(e) {
       error = e;
@@ -48,5 +55,40 @@ describe('verify API using local DID document loader', () => {
     should.exist(data.verified);
     data.verified.should.be.a('boolean');
     data.verified.should.be.true;
+  });
+  it('verifies a credential with a verifierId and a referenceId', async () => {
+    const {challenge, domain} = helpers;
+    let error;
+    let result;
+    const {v1DidDoc: credentialSignerDid, signingKey: credentialSigningKey} =
+      await helpers.generateDid();
+    const {
+      v1DidDoc: presentationSignerDid,
+      signingKey: presentationSigningKey
+    } = await helpers.generateDid();
+    loader.documents.set(credentialSignerDid.id, credentialSignerDid.doc);
+    loader.documents.set(presentationSignerDid.id, presentationSignerDid.doc);
+    const {presentation} = await helpers.generatePresentation(
+      {challenge, domain, credentialSigningKey, presentationSigningKey});
+    const url = urls.verification({verifiedId: 'foo', referenceId: 'bar'});
+    try {
+      result = await axios({
+        httpsAgent: new https.Agent({rejectUnauthorized: strictSSL}),
+        data: {challenge, domain, presentation},
+        method: 'POST',
+        url
+      });
+    } catch(e) {
+      error = e;
+    }
+    should.not.exist(error);
+    should.exist(result.data);
+    const {data} = result;
+    should.exist(data);
+    data.should.be.an('object');
+    should.exist(data.verified);
+    data.verified.should.be.a('boolean');
+    data.verified.should.be.true;
+
   });
 });
