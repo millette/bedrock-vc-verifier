2e8c865da04f11d6d5882b19f6ab94f4243b36d9
Matthew Collier <mcollier@digitalbazaar.com>
2020-03-25 17:30:15 -0400

    Add presentations endpoint.

--- test/mocha/30-verifier-interop.js
+++ test/mocha/30-verifier-interop.js
@@ -3,9 +3,12 @@
  */
 'use strict';
 
-const {config} = require('bedrock');
+const {config, util: {clone}} = require('bedrock');
 const {create} = require('apisauce');
 const {httpsAgent} = require('bedrock-https-agent');
+const vc = require('vc-js');
+const {Ed25519KeyPair} = require('crypto-ld');
+const {suites: {Ed25519Signature2018}} = require('jsonld-signatures');
 
 const api = create({
   baseURL: `${config.server.baseUri}/verifier`,
@@ -17,14 +20,22 @@ const api = create({
 // https://www.w3.org/2018/credentials/examples/v1
 const mockCredential = require('./mock-credential');
 
+const mockPresentationSigningKey = {
+  type: 'Ed25519VerificationKey2018',
+  // eslint-disable-next-line max-len
+  privateKeyBase58: '2NHZPVtqrTZ1FDDkjWT2yALQ7PxifWPVKbupzPdKqpHBzCZSwigPboHDnXxmQSJ6SdZnjnskLwXurzKkSQmQS945',
+  publicKeyBase58: 'G6RxBxsmPtk2dnGrJmGBuRXv1FejiCAYKAQ139wDNZRs'
+};
+
 describe('Interop Verifier API', () => {
   describe('credentials endpoint', () => {
     it('verifies a valid credential', async () => {
+      const verifiableCredential = clone(mockCredential);
       let error;
       let result;
       try {
         result = await api.post('/credentials', {
-          verifiableCredential: mockCredential
+          verifiableCredential
         });
       } catch(e) {
         error = e;
@@ -44,7 +55,7 @@ describe('Interop Verifier API', () => {
       let error;
       let result;
 
-      const badCredential = Object.assign({}, mockCredential);
+      const badCredential = clone(mockCredential);
 
       // change the degree name
       badCredential.credentialSubject.degree.name =
@@ -52,7 +63,7 @@ describe('Interop Verifier API', () => {
 
       try {
         result = await api.post('/credentials', {
-          verifiableCredential: mockCredential
+          verifiableCredential: badCredential
         });
       } catch(e) {
         error = e;
@@ -69,6 +80,57 @@ describe('Interop Verifier API', () => {
       check.verified.should.be.false;
       // the signature is no longer valid because the data was changed
       check.error.message.should.equal('Invalid signature.');
+    });
+  });
+
+  describe('presentations endpoint', () => {
+    it('verifies a valid presentation', async () => {
+      const verifiableCredential = clone(mockCredential);
+      const presentation = vc.createPresentation({
+        // FIXME: is a holder required?
+        // holder: 'foo',
+        id: 'urn:uuid:3e793029-d699-4096-8e74-5ebd956c3137',
+        verifiableCredential
+      });
+
+      const presentationSigningKey = new Ed25519KeyPair(
+        mockPresentationSigningKey);
+      const fingerprint = presentationSigningKey.fingerprint();
+      const verificationMethod = `did:key:${fingerprint}#${fingerprint}`;
+
+      const suite = new Ed25519Signature2018({
+        verificationMethod,
+        signer: presentationSigningKey.signer(),
+      });
+
+      const challenge = 'acdbba77-9b5f-4079-887b-97e7eda06081';
+      await vc.signPresentation({presentation, suite, challenge});
+
+      let error;
+      let result;
+      try {
+        result = await api.post('/presentations', {
+          presentation
+        });
+      } catch(e) {
+        error = e;
+      }
+      should.not.exist(error);
+      // apisauce API does not throw it puts errors in `result.problem`
+      should.not.exist(result.problem);
+      should.exist(result.data.checks);
+
+      const {checks} = result.data;
+      checks.should.be.an('array');
+      checks.should.have.length(2);
+      const [check1, check2] = checks;
+      check1.verified.should.be.a('boolean');
+      check1.verified.should.be.true;
+      // check2.verified.should.be.a('boolean');
+      // check2.verified.should.be.true;
+    });
+    it.skip('does not verify an invalid presentation', async () => {
+
     });
   });
 });
