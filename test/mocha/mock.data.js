f0e90dd12c499ba389765510d45c21b989836cfb
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-05-05 10:28:11 -0400

    Update to use ed25519-2020 signature suite.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /*!
 * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
 */
@@ -15,58 +16,58 @@ const didContexts = [
 
 const credentials = mock.credentials = {};
 credentials.alpha = {
-  "@context": [
+  '@context': [
     constants.CREDENTIALS_CONTEXT_V1_URL, {
-      "ex1": "https://example.com/examples/v1",
-      "AlumniCredential": "ex1:AlumniCredential",
-      "alumniOf": "ex1:alumniOf"
+      ex1: 'https://example.com/examples/v1',
+      AlumniCredential: 'ex1:AlumniCredential',
+      alumniOf: 'ex1:alumniOf'
     }
   ],
-  "id": "http://example.edu/credentials/58473",
-  "type": ["VerifiableCredential", "AlumniCredential"],
-  "issuer": "did:test:issuer:foo",
-  "issuanceDate": new Date().toISOString(),
-  "credentialSubject": {
-    "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-    "alumniOf": "Example University"
+  id: 'http://example.edu/credentials/58473',
+  type: ['VerifiableCredential', 'AlumniCredential'],
+  issuer: 'did:test:issuer:foo',
+  issuanceDate: new Date().toISOString(),
+  credentialSubject: {
+    id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
+    alumniOf: 'Example University'
   }
 };
 
 const presentations = mock.presentations = {};
 
 presentations.alpha = {
-  "@context": [constants.CREDENTIALS_CONTEXT_V1_URL],
-  "type": ["VerifiablePresentation"],
-  "verifiableCredential": [],
+  '@context': [constants.CREDENTIALS_CONTEXT_V1_URL],
+  type: ['VerifiablePresentation'],
+  verifiableCredential: [],
 };
 
 const privateDidDocuments = mock.privateDidDocuments = {};
 
 privateDidDocuments.alpha = {
-  "@context": didContexts,
-  "id": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY",
-  "authentication": [
+  '@context': didContexts,
+  id: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY',
+  authentication: [
     {
-      "id": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY#z279jeddPcVScp2qcA476nxuQnZGnmBHcXSKWgNusrT1u1V1",
-      "type": "Ed25519VerificationKey2018",
-      "controller": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY",
-      "publicKeyBase58": "2vXXVcAkogFwWGBHsyU1KCJrsFJLtuE8xnzyVNwmhhdq"
+      id: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY#z279jeddPcVScp2qcA476nxuQnZGnmBHcXSKWgNusrT1u1V1',
+      type: 'Ed25519VerificationKey2018',
+      controller: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY',
+      publicKeyBase58: '2vXXVcAkogFwWGBHsyU1KCJrsFJLtuE8xnzyVNwmhhdq'
     }
   ],
-  "capabilityDelegation": [
+  capabilityDelegation: [
     {
-      "id": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY#z279odRyQVywHaU723iXRVncxmd4ELNzCL5gGfcQgDVg6mhV",
-      "type": "Ed25519VerificationKey2018",
-      "controller": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY",
-      "publicKeyBase58": "6uKsWVfFUShCv9qiCgHisBNeJpW3UhsVinEUHjzRuTrK"
+      id: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY#z279odRyQVywHaU723iXRVncxmd4ELNzCL5gGfcQgDVg6mhV',
+      type: 'Ed25519VerificationKey2018',
+      controller: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY',
+      publicKeyBase58: '6uKsWVfFUShCv9qiCgHisBNeJpW3UhsVinEUHjzRuTrK'
     }
   ],
-  "capabilityInvocation": [
+  capabilityInvocation: [
     {
-      "id": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY#z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY",
-      "type": "Ed25519VerificationKey2018",
-      "controller": "did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY",
-      "publicKeyBase58": "GZDzPsdkB4ca1ELMHs4bd4Lj2sS53g77di1C4YhQobQN"
+      id: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY#z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY',
+      type: 'Ed25519VerificationKey2018',
+      controller: 'did:v1:test:nym:z279yHL6HsxRzCPU78DAWgZVieb8xPK1mJKJBbP8T2CezuFY',
+      publicKeyBase58: 'GZDzPsdkB4ca1ELMHs4bd4Lj2sS53g77di1C4YhQobQN'
     }
   ]
 };
