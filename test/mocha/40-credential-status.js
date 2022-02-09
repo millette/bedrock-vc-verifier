3a0c6ebcf0808d8090fd9303cf565bd20da597da
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-02-09 11:04:32 -0500

    Auto assign port number in test.

--- test/mocha/40-credential-status.js
+++ test/mocha/40-credential-status.js
@@ -30,14 +30,136 @@ const encodedList100KWith50KthRevoked =
 const key = fs.readFileSync(__dirname + '/key.pem');
 const cert = fs.readFileSync(__dirname + '/cert.pem');
 
-const PORT = 9001;
-const BASE_URL = `https://localhost:${PORT}`;
+let slCredential;
+let unsignedCredentialSl2021Type;
+let revokedSlCredential;
+let revokedUnsignedCredential;
+let rlCredential;
+let unsignedCredentialRL2020Type;
+let revokedRlCredential;
+let revokedUnsignedCredential2;
 
 function _startServer({app}) {
   return new Promise(resolve => {
     const server = https.createServer({key, cert}, app);
-    server.listen(PORT, () => {
+    server.listen(() => {
+      const {port} = server.address();
+      const BASE_URL = `https://localhost:${port}`;
       console.log(`Test server listening at ${BASE_URL}`);
+
+      // Status List 2021 Credential
+      slCredential = {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          VC_SL_CONTEXT_URL
+        ],
+        id: `${BASE_URL}/status/1`,
+        issuer: 'did:key:z6Mktpn6cXks1PBKLMgZH2VaahvCtBMF6K8eCa7HzrnuYLZv',
+        issuanceDate: '2022-01-10T04:24:12.164Z',
+        type: ['VerifiableCredential', 'StatusList2021Credential'],
+        credentialSubject: {
+          id: `${BASE_URL}/status/1#list`,
+          type: 'RevocationList2021',
+          encodedList: encodedList100k
+        }
+      };
+
+      // Unsigned 2021 Credential
+      unsignedCredentialSl2021Type = {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          VC_SL_CONTEXT_URL,
+          'https://w3id.org/security/suites/ed25519-2020/v1'
+        ],
+        id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
+        type: ['VerifiableCredential', 'example:TestCredential'],
+        credentialSubject: {
+          id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
+          'example:test': 'foo'
+        },
+        credentialStatus: {
+          id: `${BASE_URL}/status/1#67342`,
+          type: 'RevocationList2021Status',
+          statusListIndex: '67342',
+          statusListCredential: slCredential.id
+        },
+        issuer: slCredential.issuer,
+      };
+
+      // Revoked Status List 2021 Credential
+      revokedSlCredential = clone(slCredential);
+
+      revokedSlCredential.id = `${BASE_URL}/status/2`,
+      revokedSlCredential.credentialSubject.encodedList =
+    encodedList100KWith50KthRevoked;
+      revokedSlCredential.credentialSubject.id = `${BASE_URL}/status/2#list`;
+
+      // Revoked Unsigned 2021 Credential
+      revokedUnsignedCredential = clone(unsignedCredentialSl2021Type);
+      revokedUnsignedCredential.credentialStatus.id =
+    `${revokedSlCredential.id}#50000`;
+      revokedUnsignedCredential.credentialStatus.statusListIndex = 50000;
+      revokedUnsignedCredential.credentialStatus.statusListCredential =
+    `${revokedSlCredential.id}`;
+      revokedUnsignedCredential.issuer = revokedSlCredential.issuer;
+
+      // Revocation List 2020 Credential
+      rlCredential = {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          VC_RL_CONTEXT_URL
+        ],
+        id: `${BASE_URL}/status/3`,
+        issuer: 'did:key:z6Mktpn6cXks1PBKLMgZH2VaahvCtBMF6K8eCa7HzrnuYLZv',
+        issuanceDate: '2022-01-10T04:24:12.164Z',
+        type: ['VerifiableCredential', 'RevocationList2020Credential'],
+        credentialSubject: {
+          id: `${BASE_URL}/status/3#list`,
+          type: 'RevocationList2020',
+          encodedList: encodedList100k
+        }
+      };
+
+      // Unsigned 2020 Credential
+      unsignedCredentialRL2020Type = {
+        '@context': [
+          'https://www.w3.org/2018/credentials/v1',
+          VC_RL_CONTEXT_URL,
+          'https://w3id.org/security/suites/ed25519-2020/v1'
+        ],
+        id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
+        type: ['VerifiableCredential', 'example:TestCredential'],
+        credentialSubject: {
+          id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
+          'example:test': 'foo'
+        },
+        issuanceDate: '2022-01-11T19:23:24Z',
+        credentialStatus: {
+          id: `${BASE_URL}/status/3#67342`,
+          type: 'RevocationList2020Status',
+          revocationListIndex: '67342',
+          revocationListCredential: rlCredential.id
+        },
+        issuer: rlCredential.issuer,
+      };
+
+      // Revoked Revocation List 2020 Credential
+      revokedRlCredential = clone(rlCredential);
+
+      revokedRlCredential.id = `${BASE_URL}/status/4`,
+      revokedRlCredential.credentialSubject.encodedList =
+    encodedList100KWith50KthRevoked;
+      revokedRlCredential.credentialSubject.id = `${BASE_URL}/status/4#list`;
+
+      // Revoked Unsigned 2020 Credential
+      revokedUnsignedCredential2 = clone(unsignedCredentialRL2020Type);
+      revokedUnsignedCredential2.credentialStatus.id =
+    `${revokedRlCredential.id}#50000`;
+      revokedUnsignedCredential2.credentialStatus.revocationListIndex = 50000;
+      revokedUnsignedCredential2.credentialStatus.revocationListCredential =
+    `${revokedRlCredential.id}`;
+      revokedUnsignedCredential2.issuer = revokedRlCredential.issuer;
+
       return resolve(server);
     });
   });
@@ -45,117 +167,6 @@ function _startServer({app}) {
 
 const app = express();
 app.use(express.json());
-
-// Status List 2021 Credential
-let slCredential = {
-  '@context': [
-    'https://www.w3.org/2018/credentials/v1',
-    VC_SL_CONTEXT_URL
-  ],
-  id: `${BASE_URL}/status/1`,
-  issuer: 'did:key:z6Mktpn6cXks1PBKLMgZH2VaahvCtBMF6K8eCa7HzrnuYLZv',
-  issuanceDate: '2022-01-10T04:24:12.164Z',
-  type: ['VerifiableCredential', 'StatusList2021Credential'],
-  credentialSubject: {
-    id: `${BASE_URL}/status/1#list`,
-    type: 'RevocationList2021',
-    encodedList: encodedList100k
-  }
-};
-
-// Unsigned 2021 Credential
-const unsignedCredentialSl2021Type = {
-  '@context': [
-    'https://www.w3.org/2018/credentials/v1',
-    VC_SL_CONTEXT_URL,
-    'https://w3id.org/security/suites/ed25519-2020/v1'
-  ],
-  id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
-  type: ['VerifiableCredential', 'example:TestCredential'],
-  credentialSubject: {
-    id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
-    'example:test': 'foo'
-  },
-  credentialStatus: {
-    id: `${BASE_URL}/status/1#67342`,
-    type: 'RevocationList2021Status',
-    statusListIndex: '67342',
-    statusListCredential: slCredential.id
-  },
-  issuer: slCredential.issuer,
-};
-
-// Revoked Status List 2021 Credential
-let revokedSlCredential = clone(slCredential);
-
-revokedSlCredential.id = `${BASE_URL}/status/2`,
-revokedSlCredential.credentialSubject.encodedList =
-  encodedList100KWith50KthRevoked;
-revokedSlCredential.credentialSubject.id = `${BASE_URL}/status/2#list`;
-
-const revokedUnsignedCredential = clone(unsignedCredentialSl2021Type);
-revokedUnsignedCredential.credentialStatus.id =
-  `${revokedSlCredential.id}#50000`;
-revokedUnsignedCredential.credentialStatus.statusListIndex = 50000;
-revokedUnsignedCredential.credentialStatus.statusListCredential =
-  `${revokedSlCredential.id}`;
-revokedUnsignedCredential.issuer = revokedSlCredential.issuer;
-
-// Revocation List 2020 Credential
-let rlCredential = {
-  '@context': [
-    'https://www.w3.org/2018/credentials/v1',
-    VC_RL_CONTEXT_URL
-  ],
-  id: `${BASE_URL}/status/3`,
-  issuer: 'did:key:z6Mktpn6cXks1PBKLMgZH2VaahvCtBMF6K8eCa7HzrnuYLZv',
-  issuanceDate: '2022-01-10T04:24:12.164Z',
-  type: ['VerifiableCredential', 'RevocationList2020Credential'],
-  credentialSubject: {
-    id: `${BASE_URL}/status/3#list`,
-    type: 'RevocationList2020',
-    encodedList: encodedList100k
-  }
-};
-
-// Unsigned 2020 Credential
-const unsignedCredentialRL2020Type = {
-  '@context': [
-    'https://www.w3.org/2018/credentials/v1',
-    VC_RL_CONTEXT_URL,
-    'https://w3id.org/security/suites/ed25519-2020/v1'
-  ],
-  id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
-  type: ['VerifiableCredential', 'example:TestCredential'],
-  credentialSubject: {
-    id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
-    'example:test': 'foo'
-  },
-  issuanceDate: '2022-01-11T19:23:24Z',
-  credentialStatus: {
-    id: `${BASE_URL}/status/3#67342`,
-    type: 'RevocationList2020Status',
-    revocationListIndex: '67342',
-    revocationListCredential: rlCredential.id
-  },
-  issuer: rlCredential.issuer,
-};
-
-// Revoked Revocation List 2020 Credential
-let revokedRlCredential = clone(rlCredential);
-
-revokedRlCredential.id = `${BASE_URL}/status/4`,
-revokedRlCredential.credentialSubject.encodedList =
-  encodedList100KWith50KthRevoked;
-revokedRlCredential.credentialSubject.id = `${BASE_URL}/status/4#list`;
-
-const revokedUnsignedCredential2 = clone(unsignedCredentialRL2020Type);
-revokedUnsignedCredential2.credentialStatus.id =
-  `${revokedRlCredential.id}#50000`;
-revokedUnsignedCredential2.credentialStatus.revocationListIndex = 50000;
-revokedUnsignedCredential2.credentialStatus.revocationListCredential =
-  `${revokedRlCredential.id}`;
-revokedUnsignedCredential2.issuer = revokedRlCredential.issuer;
 
 // mount the test routes
 app.get('/status/1',
