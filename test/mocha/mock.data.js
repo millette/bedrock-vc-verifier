97072483b012160af23097e0bdb8435a17752172
Dave Longley <dlongley@digitalbazaar.com>
2022-02-20 19:20:25 -0500

    Remove unused mock data.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -25,30 +25,3 @@ mock.productIdMap = new Map([
 ]);
 
 mock.baseUrl = config.server.baseUri;
-
-const credentials = mock.credentials = {};
-credentials.alpha = {
-  '@context': [
-    CREDENTIALS_CONTEXT_V1_URL, {
-      ex1: 'https://example.com/examples/v1',
-      AlumniCredential: 'ex1:AlumniCredential',
-      alumniOf: 'ex1:alumniOf'
-    }
-  ],
-  id: 'http://example.edu/credentials/58473',
-  type: ['VerifiableCredential', 'AlumniCredential'],
-  issuer: 'did:test:issuer:foo',
-  issuanceDate: new Date().toISOString(),
-  credentialSubject: {
-    id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
-    alumniOf: 'Example University'
-  }
-};
-
-const presentations = mock.presentations = {};
-
-presentations.alpha = {
-  '@context': [CREDENTIALS_CONTEXT_V1_URL],
-  type: ['VerifiablePresentation'],
-  verifiableCredential: [],
-};
